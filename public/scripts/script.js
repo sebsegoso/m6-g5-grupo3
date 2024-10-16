b1006ad624097db99e118e4b4cd2d5fa76a2bb82
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2024-10-15 22:01:07 -0300

    paginador

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,11 @@
 async function loadPokemon(gen) {
-    try {
-        const response = await fetch(`/pokemones?gen=${gen}`);
-        const pokemones = await response.json();
-        const gallery = document.getElementById('pokemon-gallery');
-        gallery.innerHTML = pokemones.map(pokemon => `
+  try {
+    const response = await fetch(`/pokemones?gen=${gen}`);
+    const pokemones = await response.json();
+    const gallery = document.getElementById("pokemon-gallery");
+    gallery.innerHTML = pokemones
+      .map(
+        (pokemon) => `
             <div class="col">
                 <div class="card h-100 text-center">
                     <img src="${pokemon.image}" class="card-img-top" alt="${pokemon.name}">
@@ -12,23 +14,41 @@ async function loadPokemon(gen) {
                     </div>
                 </div>
             </div>
-        `).join('');
-    } catch (error) {
-        console.error('Error cargando Pokémon:', error);
-    }
+        `
+      )
+      .join("");
+    setActivePage(gen);
+    window.scroll(0, 0);
+  } catch (error) {
+    console.error("Error cargando Pokémon:", error);
+  }
 }
 
+function setActivePage(newPage) {
+  const paginator = document.getElementById("gen-paginator");
 
-document.getElementById('gen1').addEventListener('click', () => loadPokemon(1));
-document.getElementById('gen2').addEventListener('click', () => loadPokemon(2));
-document.getElementById('gen3').addEventListener('click', () => loadPokemon(3));
-document.getElementById('gen4').addEventListener('click', () => loadPokemon(4));
-document.getElementById('gen5').addEventListener('click', () => loadPokemon(5));
-document.getElementById('gen6').addEventListener('click', () => loadPokemon(6));
-document.getElementById('gen7').addEventListener('click', () => loadPokemon(7));
-document.getElementById('gen8').addEventListener('click', () => loadPokemon(8));
-document.getElementById('gen9').addEventListener('click', () => loadPokemon(9));
+  if (!paginator) console.error("Paginador no encontrado");
+  const exBtn = paginator.querySelector(".active");
+  if (exBtn) {
+    exBtn.classList.remove("active");
+  }
+  const newId = `gen${newPage}`;
+  if (exBtn?.id == newId) return;
 
+  const btn = paginator.querySelector(`#${newId}`);
+
+  if (!btn) return;
+  btn.classList.add("active");
+}
+
+document.getElementById("gen1").addEventListener("click", () => loadPokemon(1));
+document.getElementById("gen2").addEventListener("click", () => loadPokemon(2));
+document.getElementById("gen3").addEventListener("click", () => loadPokemon(3));
+document.getElementById("gen4").addEventListener("click", () => loadPokemon(4));
+document.getElementById("gen5").addEventListener("click", () => loadPokemon(5));
+document.getElementById("gen6").addEventListener("click", () => loadPokemon(6));
+document.getElementById("gen7").addEventListener("click", () => loadPokemon(7));
+document.getElementById("gen8").addEventListener("click", () => loadPokemon(8));
+document.getElementById("gen9").addEventListener("click", () => loadPokemon(9));
 
 window.onload = () => loadPokemon(1);
-
